4fad184260261a51a64a48d32537d5c3e12ec23a
Rothelien Caryle Akai S. Amparo <59528766+rcasamparo-tip@users.noreply.github.com>
2022-03-13 19:47:56 +0800

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 #webhosting deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
