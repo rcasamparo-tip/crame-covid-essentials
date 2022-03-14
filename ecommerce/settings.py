f2c90ab284e6f499437c8d33e4128288fbd89077
Rothelien Caryle Akai S. Amparo <59528766+rcasamparo-tip@users.noreply.github.com>
2022-03-15 01:11:25 +0800

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_URL = 'static/images/', '/checkout/static/images/', '/cart/static/images/'
+MEDIA_URL = 'static/images/', 'checkout/static/images/', 'cart/static/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
